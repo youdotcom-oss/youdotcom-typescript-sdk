374cdde13b93bc3d04617161336bfc2296dd05dc
Kevin Malek <kevin@you.com>
2025-12-08 11:24:33 -0800

    Fixed tests

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -58,8 +58,8 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "0.0.1",
-  sdkVersion: "0.10.15",
+  sdkVersion: "0.10.16",
   genVersion: "2.774.0",
   userAgent:
-    "speakeasy-sdk/typescript 0.10.15 2.774.0 0.0.1 @youdotcom-oss/sdk",
+    "speakeasy-sdk/typescript 0.10.16 2.774.0 0.0.1 @youdotcom-oss/sdk",
 } as const;
